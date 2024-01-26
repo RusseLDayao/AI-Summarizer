61ce24d148de91ce6f6e0452c6308aac93cf5f1d
RusseLDayao <russeldayao@gmail.com>
2024-01-26 15:48:42 +0800

    Second commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { landLogo } from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center
+    items-center flex-col'>
+      <nav className='flex justify-between  
+      items-center flex-col'>
+          <img src={ landLogo }/>
+      </nav>
+    </header>
   )
 }
 
